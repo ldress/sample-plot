f8fb75041e7303d01729c87081f6822a1ea3eedd
ldress <ldress@uw.edu>
2024-12-04 22:52:37 -0800

    minor edits

--- sampleplot/utils/data_loader.py
+++ sampleplot/utils/data_loader.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import os
 
 def load_data(data_path):
-    data = pd.read_csv(data_path)
+    # Get the absolute path to the data file
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.abspath(os.path.join(base_dir, '..', '..', data_path))
+    
+    # Load the data
+    data = pd.read_csv(full_path)
     return data
